c924df19eb1c77a0413c580044063f926a684b02
Victor Roemer <viroemer@cisco.com>
2015-05-17 00:58:08 -0400

    Now it compares the current version to the install

--- index.js
+++ index.js
@@ -7,6 +7,44 @@
 var http = require('https');
 var jsdom = require('jsdom');
 
+// Taken from StackOverflow
+// http://stackoverflow.com/questions/6832596/how-to-compare-software-version-number-using-js-only-number#answer-6832706
+var version_compare = function(a,b) {
+    if (a === b) {
+        return 0;
+    }
+
+    var a_pts = a.split(".");
+    var b_pts = b.split(".");
+
+    var len = Math.min(a_pts.length, b_pts.length);
+
+    // loop while the components are equal
+    for (var i = 0; i < len; i++) {
+        // A bigger than B
+        if (parseInt(a_pts[i]) > parseInt(b_pts[i])) {
+            return 1;
+        }
+
+        // B bigger than A
+        if (parseInt(a_pts[i]) < parseInt(b_pts[i])) {
+            return -1;
+        }
+    }
+
+    // If one's a prefix of the other, the longer one is greater.
+    if (a_pts.length > b_pts.length) {
+        return 1;
+    }
+
+    if (a_pts.length < b_pts.length) {
+        return -1;
+    }
+
+    // Otherwise they are the same.
+    return 0;
+}
+
 http.request({ host: 'nodejs.org', path: '/' }, function(resp) {
     var body = '';
 
@@ -17,6 +55,16 @@ http.request({ host: 'nodejs.org', path: '/' }, function(resp) {
     resp.on('end', function() {  
         var parse = jsdom.jsdom;
         var doc = parse(body);
-        console.log(doc.querySelector('.version').innerHTML);
+
+        var str = doc.querySelector('.version').innerHTML;
+        var curr = str.replace( /Current Version:\s*v/, '' );
+        var inst = process.version.replace('^v', '');
+    
+        var r = version_compare(curr, inst);
+
+        if ( r == 1 ) {
+            console.log("A newer version of NodeJS is available!");
+            console.log("Current: " + curr + "\nInstall: " + inst );
+        }
     });
 }).end();
